7c6504f1ea5ee49f54b3161e08d9cd5923dc33bf
Cris Mihalache <me@f3rno.com>
2019-06-15 22:33:19 +0700

    (feature) WIP defines basic schema, pulls in audit_gaps/sync_range [unmodified]

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const syncTradesRange = require('./lib/models/trade/sync_range')
-const tradeSchema = require('./lib/models/trade/schema')
+const syncCandlesRange = require('./lib/models/candle/sync_range')
+const auditCandleGaps = require('./lib/models/candle/audit_gaps')
 
+// TODO: Add order submit/etc logic
 module.exports = {
-  models: {
-    trade: {
-      schema: tradeSchema,
-      syncRange: syncTradesRange,
+  schema: {
+    Trade: {
+      schema: null, // use default
+      schemaExchangeData: {
+        id: Number,
+      },
+
+      methods: {
+        syncRange: syncTradesRange,
+      },
     },
+
+    Candle: {
+      schema: null, // use default
+      schemaExchangeData: {
+        type: String,
+      },
+
+      methods: {
+        syncRange: syncCandlesRange,
+        auditGaps: auditCandleGaps,
+      }
+    }
   },
 }
