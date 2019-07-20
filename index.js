f622f26c61f75e766f590e8c6806cb18f0685ff2
Cris Mihalache <me@f3rno.com>
2019-07-21 02:57:12 +0700

    (feature) add algo order adapter

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+'use strict'
+
+const AOAdapter = require('./lib/ao_adapter')
 const syncTradesRange = require('./lib/models/trade/sync_range')
 const syncCandlesRange = require('./lib/models/candle/sync_range')
 const auditCandleGaps = require('./lib/models/candle/audit_gaps')
 
-// TODO: Add order submit/etc logic
 module.exports = {
+  AOAdapter,
+
   schema: {
     Trade: {
       schemaExchangeData: {
